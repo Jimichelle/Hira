aa02d315d9c632cc65347f7467274924eda82e04
Aymerick Jakob Miaro ANDRIAMBELO <157814646+aymadr@users.noreply.github.com>
2024-10-08 18:16:38 +0200

    bien soigné

--- php/header_login.php
+++ php/header_login.php
@@ -1,7 +1,7 @@
 <div class="dropdown">
         <img src="public/avatar-placeholder.png" alt="Avatar" class="rounded-circle" height="40" width="40" data-bs-toggle="dropdown">
         <ul class="dropdown-menu dropdown-menu-end">
-          <li><a class="dropdown-item" href="#">Mon profil</a></li>
+          <li><a class="dropdown-item" href="admin.php">Mon profil</a></li>
           <li><a class="dropdown-item" href="#">Paramètres</a></li>
           <li><hr class="dropdown-divider"></li>
           <li><a class="dropdown-item" href="disconnect.php">Déconnexion</a></li>
